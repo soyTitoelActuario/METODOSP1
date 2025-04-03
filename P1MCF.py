a74b01d57ddf1e47875fc67fe356d2946dea6031
Antonio Aldair Pérez Galindo <113151775+soyTitoelActuario@users.noreply.github.com>
2025-04-03 17:24:35 -0600

    Update P1MCF.py

--- P1MCF.py
+++ P1MCF.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import plotly.graph_objects as go
-import plotly.express as px
+import plotly as go
+import plotly as px
 from datetime import datetime, timedelta
 from scipy.optimize import minimize
 import pandas as pd
